b50f5fe17ae76955ef96567993d29a1998252f4e
Odaymon <stevenpascaran@gmail.com>
2018-09-05 23:28:59 +0800

    Initial commit

---
+++ app/src/main/java/com/pascaran/stevenjohn/constraint.java
@@ -0,0 +1,15 @@
+package com.pascaran.stevenjohn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class constraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+        //setContentView(R.layout.activity_relative);
+        //setContentView(R.layout.activity_linear);
+    }
+}
